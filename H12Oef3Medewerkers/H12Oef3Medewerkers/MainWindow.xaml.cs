ec2955702df95664d9119cd6394eba919c43432b
Indy Iri <indy.iri@student.pxl.be>
2024-12-12 13:51:45 +0100

    Afgewerkt

--- H12Oef3Medewerkers/H12Oef3Medewerkers/MainWindow.xaml.cs
+++ H12Oef3Medewerkers/H12Oef3Medewerkers/MainWindow.xaml.cs
@@ -20,5 +20,69 @@ namespace H12Oef3Medewerkers
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            errorLabel.Content = "Geen error";
+            updateSalaryButton.IsEnabled = false;
+            salaryTextBox.IsEnabled = false;
+
+            Output();
+        }
+
+        string[] employees = {"Kristof", "Sander", "Koen"};
+        string[] employeeNumbers = {"M01", "M02", "M03"};
+        decimal[] salaries = {0, 0, 0};
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (summaryListBox.SelectedIndex != -1)
+            {
+                updateSalaryButton.IsEnabled = true;
+                salaryTextBox.IsEnabled = true;
+                salaryTextBox.Text = salaries[summaryListBox.SelectedIndex].ToString();
+            }
+            else
+            {
+                updateSalaryButton.IsEnabled = false;
+                salaryTextBox.IsEnabled = false;
+            }
+        }
+
+        private void updateSalaryButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (summaryListBox.SelectedIndex != -1)
+            {
+                string inputNewSalary = salaryTextBox.Text;
+                decimal newSalary;
+
+                bool isInputValid = decimal.TryParse(inputNewSalary, out newSalary);
+
+                if (isInputValid == false || newSalary < 0)
+                {
+                    errorLabel.Content = "U moet een geldig salaris ingeven";
+                    salaryTextBox.Focus();
+                }
+                else
+                {
+                    salaries[summaryListBox.SelectedIndex] = newSalary;
+                    errorLabel.Content = "Geen error";
+                }
+
+                summaryListBox.Items.Clear();
+
+                Output();
+            }
+        }
+
+        private void Output()
+        {
+            for (int i = 0; i < employees.Length; i++)
+            {
+                ListBoxItem listBoxItem = new ListBoxItem();
+                listBoxItem.Content = $"{employees[i]} - {employeeNumbers[i]} - €{salaries[i]}";
+                summaryListBox.Items.Add(listBoxItem);
+            }
+        }
     }
 }
